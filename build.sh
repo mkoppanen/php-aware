53dbaf26ed113a244f6465e32257d3a1d1c4840e
Mikko Koppanen <mkoppanen@php.net>
2010-11-05 10:53:12 +0000

    Revert the change in build.sh

--- build.sh
+++ build.sh
@@ -9,16 +9,16 @@ export CFLAGS="-Wall -g"
 phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
 cd storage/files && \
 phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
-#cd ../snmp && \
-#phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
-#cd ../tokyo && \
-#phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
-#cd ../stomp && \
-#phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
-#cd ../email && \
-#phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
-#cd ../spread && \
-#phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
+cd ../snmp && \
+phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
+cd ../tokyo && \
+phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
+cd ../stomp && \
+phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
+cd ../email && \
+phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
+cd ../spread && \
+phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
 cd ../zeromq2 && \
 phpize && ./configure ${CONFIGURE_OPTS} && make clean && make && make install && \
 cd ../skeleton && \
