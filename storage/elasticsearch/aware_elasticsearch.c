78341f2f8efba09b99abc44e6c0c2f456a4d2065
Jess Portnoy <jess.portnoy@kaltura.com>
2015-12-26 19:12:03 +0000

    first commit for elasticsearch backend.

---
+++ storage/elasticsearch/aware_elasticsearch.c
@@ -0,0 +1,287 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 5 / aware                                                |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2009 Mikko Koppanen                                    |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Mikko Kopppanen <mkoppanen@php.net>                          |
+   +----------------------------------------------------------------------+
+*/
+
+#include "php_aware_elasticsearch.h"
+
+/* holder for curl fetch */
+struct curl_fetch_st {
+    char *payload;
+    size_t size;
+};
+
+/* callback for curl fetch */
+size_t curl_callback (void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;                             /* calculate buffer size */
+    struct curl_fetch_st *p = (struct curl_fetch_st *) userp;   /* cast pointer to fetch struct */
+
+    /* expand buffer */
+    p->payload = (char *) realloc(p->payload, p->size + realsize + 1);
+
+    /* check buffer */
+    if (p->payload == NULL) {
+      /* this isn't good */
+      aware_printf("ERROR: Failed to expand buffer in curl_callback");
+      /* free buffer */
+      free(p->payload);
+      /* return */
+      return -1;
+    }
+
+    /* copy contents to buffer */
+    memcpy(&(p->payload[p->size]), contents, realsize);
+
+    /* set new buffer size */
+    p->size += realsize;
+
+    /* ensure null termination */
+    p->payload[p->size] = 0;
+
+    /* return size */
+    return realsize;
+}
+/* fetch and return url body via curl */
+CURLcode curl_fetch_url(CURL *ch, const char *url, struct curl_fetch_st *fetch) {
+    CURLcode rcode;                   /* curl result code */
+
+    /* init payload */
+    fetch->payload = (char *) calloc(1, sizeof(fetch->payload));
+
+    /* check payload */
+    if (fetch->payload == NULL) {
+        /* log error */
+        aware_printf("ERROR: Failed to allocate payload in curl_fetch_url");
+        /* return error */
+        return CURLE_FAILED_INIT;
+    }
+
+    /* init size */
+    fetch->size = 0;
+
+    /* set url to fetch */
+    curl_easy_setopt(ch, CURLOPT_URL, url);
+
+    /* set calback function */
+    curl_easy_setopt(ch, CURLOPT_WRITEFUNCTION, curl_callback);
+
+    /* pass fetch struct pointer */
+    curl_easy_setopt(ch, CURLOPT_WRITEDATA, (void *) fetch);
+
+    /* set default user agent */
+    curl_easy_setopt(ch, CURLOPT_USERAGENT, "php-aware-elasticsearch/1.0");
+
+    /* set timeout */
+    curl_easy_setopt(ch, CURLOPT_TIMEOUT, 5);
+
+    /* enable location redirects */
+    curl_easy_setopt(ch, CURLOPT_FOLLOWLOCATION, 1);
+
+    /* set maximum allowed redirects */
+    curl_easy_setopt(ch, CURLOPT_MAXREDIRS, 1);
+
+    /* fetch the url */
+    rcode = curl_easy_perform(ch);
+
+    /* return */
+    return rcode;
+}
+ZEND_DECLARE_MODULE_GLOBALS(aware_elasticsearch)
+
+php_aware_storage_module php_aware_storage_module_elasticsearch = {
+	PHP_AWARE_STORAGE_MOD(elasticsearch)
+};
+
+PHP_AWARE_CONNECT_FUNC(elasticsearch)
+{
+	return AwareOperationNotSupported;
+}
+
+PHP_AWARE_GET_FUNC(elasticsearch)
+{
+	return AwareOperationNotSupported;
+}
+
+PHP_AWARE_STORE_FUNC(elasticsearch)
+{
+    zval **ppzval;
+    CURL *ch;                                               /* curl handle */
+    CURLcode rcode;                                         /* curl result code */
+
+    json_object *json;                                      /* json post body */
+    enum json_tokener_error jerr = json_tokener_success;    /* json parse error */
+
+    struct curl_fetch_st curl_fetch;                        /* curl fetch struct */
+    struct curl_fetch_st *cf = &curl_fetch;                 /* pointer to fetch struct */
+    struct curl_slist *headers = NULL;                      /* http headers to send with request */
+
+    /* url to test site */
+    char *url = "http://localhost:9200/kaltura/log";
+
+    /* init curl handle */
+    if ((ch = curl_easy_init()) == NULL) {
+        /* log error */
+        aware_printf("ERROR: Failed to create curl handle in fetch_session");
+        /* return error */
+        return 1;
+    }
+
+    /* set content type */
+    headers = curl_slist_append(headers, "Accept: application/json");
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+
+	/*
+		Error body
+	*/
+    /* set content type */
+    headers = curl_slist_append(headers, "Accept: application/json");
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+
+    /* create json object for post */
+    json = json_object_new_object();
+    //char *error_file_line;
+    //spprintf(&error_file_line, MAXPATHLEN + 256, "%s:%ld", error_filename, error_lineno);
+    /* build post data */
+    json_object_object_add(json, "file", json_object_new_string(error_filename));
+//    json_object_object_add(json, "line_number", json_object_new_int(error_lineno));
+    json_object_object_add(json, "error", json_object_new_string(Z_STRVAL_PP(ppzval)));
+    json_object_object_add(json, "userId", json_object_new_string("php-aware-elastic"));
+    //efree(error_file_line);
+
+    /* set curl options */
+    curl_easy_setopt(ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(ch, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(ch, CURLOPT_POSTFIELDS, json_object_to_json_string(json));
+
+    /* fetch page and capture return code */
+    rcode = curl_fetch_url(ch, url, cf);
+
+    /* cleanup curl handle */
+    curl_easy_cleanup(ch);
+
+    /* free headers */
+    curl_slist_free_all(headers);
+
+    /* free json object */
+    json_object_put(json);
+
+    /* check return code */
+    if (rcode != CURLE_OK || cf->size < 1) {
+        /* log error */
+        aware_printf("ERROR: Failed to fetch url (%s) - curl said: %s",
+            url, curl_easy_strerror(rcode));
+        /* return error */
+        return AwareOperationFailed;
+    }
+
+	if (zend_hash_find(Z_ARRVAL_P(event), "error_message", sizeof("error_message"), (void **) &ppzval) == SUCCESS) {
+	    return AwareOperationSuccess;
+	}
+}
+
+PHP_AWARE_GET_LIST_FUNC(elasticsearch)
+{
+	return AwareOperationNotSupported;
+}
+
+PHP_AWARE_DELETE_FUNC(elasticsearch)
+{
+	return AwareOperationNotSupported;
+}
+
+PHP_AWARE_DISCONNECT_FUNC(elasticsearch)
+{
+	return AwareOperationNotSupported;
+}
+
+PHP_INI_BEGIN()
+	STD_PHP_INI_ENTRY("aware_elasticsearch.foobar", "somevalue", PHP_INI_PERDIR, OnUpdateString, foobar, zend_aware_elasticsearch_globals, aware_elasticsearch_globals)
+PHP_INI_END()
+
+static void php_aware_elasticsearch_init_globals(zend_aware_elasticsearch_globals *aware_elasticsearch_globals)
+{
+	aware_elasticsearch_globals->foobar = NULL;	
+}
+
+/* {{{ PHP_MINIT_FUNCTION(aware_elasticsearch) */
+PHP_MINIT_FUNCTION(aware_elasticsearch) 
+{
+	AwareModuleRegisterStatus reg_status;
+	
+	ZEND_INIT_MODULE_GLOBALS(aware_elasticsearch, php_aware_elasticsearch_init_globals, NULL);
+	REGISTER_INI_ENTRIES();
+	
+	reg_status = PHP_AWARE_STORAGE_REGISTER(elasticsearch);
+	
+	switch (reg_status) 
+	{
+		case AwareModuleRegistered:	
+			AWARE_ELASTICSEARCH_G(enabled) = 1;
+		break;
+		
+		case AwareModuleFailed:
+			AWARE_ELASTICSEARCH_G(enabled) = 0;
+			return FAILURE;
+		break;
+
+		case AwareModuleNotConfigured:
+			AWARE_ELASTICSEARCH_G(enabled) = 0;
+		break;	
+	}
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MSHUTDOWN_FUNCTION(aware_elasticsearch) */
+PHP_MSHUTDOWN_FUNCTION(aware_elasticsearch)
+{
+	UNREGISTER_INI_ENTRIES();
+	return SUCCESS;
+}
+
+/* }}} */
+
+/* {{{ PHP_MINFO_FUNCTION(aware_elasticsearch) */
+PHP_MINFO_FUNCTION(aware_elasticsearch)
+{	
+	php_info_print_table_start();
+	php_info_print_table_row(2, "aware_elasticsearch storage", "enabled");
+	php_info_print_table_row(2, "aware_elasticsearch version", PHP_AWARE_ELASTICSEARCH_EXTVER);
+	php_info_print_table_end();
+
+	DISPLAY_INI_ENTRIES(); 
+}
+
+static zend_function_entry aware_elasticsearch_functions[] = {
+	{NULL, NULL, NULL}
+};
+
+zend_module_entry aware_elasticsearch_module_entry = {
+        STANDARD_MODULE_HEADER,
+        "aware_elasticsearch",
+        aware_elasticsearch_functions,
+        PHP_MINIT(aware_elasticsearch),
+        PHP_MSHUTDOWN(aware_elasticsearch),
+        NULL,
+        NULL,
+        PHP_MINFO(aware_elasticsearch),
+    	PHP_AWARE_ELASTICSEARCH_EXTVER,
+        STANDARD_MODULE_PROPERTIES
+};
+
+#ifdef COMPILE_DL_AWARE_ELASTICSEARCH
+ZEND_GET_MODULE(aware_elasticsearch)
+#endif
---
+++ storage/elasticsearch/php_aware_elasticsearch.h
@@ -0,0 +1,66 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 5 / aware                                                |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2009 Mikko Koppanen                                    |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Mikko Kopppanen <mkoppanen@php.net>                          |
+   +----------------------------------------------------------------------+
+*/
+
+#ifndef _PHP_AWARE_ELASTICSEARCH_H_
+# define _PHP_AWARE_ELASTICSEARCH_H_
+
+#define PHP_AWARE_ELASTICSEARCH_EXTVER "0.0.1-dev"
+
+#include "php.h"
+#include "php_ini.h"
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#ifdef ZTS
+# include "TSRM.h"
+#endif
+
+#include "../../php_aware.h"
+#include "../../php_aware_storage.h"
+/* json-c (https://github.com/json-c/json-c) */
+#include <json-c/json.h>
+
+/* libcurl (http://curl.haxx.se/libcurl/c) */
+#include <curl/curl.h>
+
+ZEND_BEGIN_MODULE_GLOBALS(aware_elasticsearch)
+	char *foobar;
+	zend_bool enabled;
+ZEND_END_MODULE_GLOBALS(aware_elasticsearch)
+
+ZEND_EXTERN_MODULE_GLOBALS(aware_elasticsearch)
+
+#ifdef ZTS
+# define AWARE_ELASTICSEARCH_G(v) TSRMG(aware_elasticsearch_globals_id, zend_aware_elasticsearch_globals *, v)
+#else
+# define AWARE_ELASTICSEARCH_G(v) (aware_elasticsearch_globals.v)
+#endif
+
+/* Hook into aware module */
+extern php_aware_storage_module php_aware_storage_module_elasticsearch;
+#define php_aware_storage_module_elasticsearch_ptr &php_aware_storage_module_elasticsearch
+
+/* Normal PHP entry */
+extern zend_module_entry aware_elasticsearch_module_entry;
+#define phpext_aware_elasticsearch_ptr &aware_elasticsearch_module_entry
+
+PHP_AWARE_STORAGE_FUNCS(elasticsearch);
+
+#endif
